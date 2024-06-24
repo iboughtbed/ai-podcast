38ee754f0ac8be5f216c06c2580ccc1ceee4dbfa
iboughtbed <testpythontosanzhar@gmail.com>
2024-06-24 16:56:09 +0500

    refactor: debug the feed fetch handler

--- src/app/api/podcast/fetch-feed/route.ts
+++ src/app/api/podcast/fetch-feed/route.ts
@@ -6,17 +6,26 @@ import type { Feed } from "~/types";
 
 async function handler() {
   try {
+    console.log("Here 9");
+
     const response = await fetch(env.DISCOVER_FEED_URL, {
       next: {
         revalidate: 60 * 60 * 12, // 12 hours
       },
     });
 
+    console.log("Here 17");
+
     const feed = (await response.json()) as Feed;
+
+    console.log("Here 21");
+
     const items = feed.items.map((item) => ({
       title: item.title,
       description: item.description,
     }));
+
+    console.log("Here 28");
 
     await redis.json.set("feed-items", "$", items);
 
