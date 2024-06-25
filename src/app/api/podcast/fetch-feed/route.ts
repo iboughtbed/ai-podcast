f1691daa8e9261aa1361547dcc5965b226562a32
iboughtbed <testpythontosanzhar@gmail.com>
2024-06-25 15:15:44 +0500

    fix: add user agent to fetch

--- src/app/api/podcast/fetch-feed/route.ts
+++ src/app/api/podcast/fetch-feed/route.ts
@@ -30,6 +30,8 @@ async function handler() {
           "upgrade-insecure-requests": "1",
           cookie:
             "__cflb=02DiuDyvFMmK5p9jVbVnMNSKYZhUL9aGmLHRC8T5BZ47W; pplx.visitor-id=155d6fd9-ef82-4dc0-9504-34740b367acb; _ga=GA1.1.1334287756.1719231471; next-auth.csrf-token=ffde0fe1f254e846e4ee8e10d1152074d6eac79e841eb761a16bda9b1c573ecc%7C58f85e5901b9e5eefd3e1478f8ca6fe80faad7b3bf1cafa6ac3ead8ca2b41006; next-auth.callback-url=https%3A%2F%2Fwww.perplexity.ai%2Fapi%2Fauth%2Fsignin-callback%3Fredirect%3Dhttps%253A%252F%252Fwww.perplexity.ai; _ga_SH9PRBQG23=GS1.1.1719305388.5.1.1719305427.0.0.0; AWSALB=+WI8Wx2j/XyUk8QfsJHA/758M3p9zmV0AWjJwdqjZmVw5QkLNReMN6iLNHJJrnW/ZyxBtb3zjteTo53Msadc0Qz8Ud1AHjaw7sAchyOxCz7kyCU9r5pBeRT9SEMFuEFVXjQ9jXxjn40TTIy3lqTtPc10h0VI1TFTons/u6oNmcZ8x/m3EXWFYxUDvHvNTlIBw0Cpn/eimo58LIxlpH2I/RNo1RMvlOWzQh0BAtuFvrN8N7p+hPqXRORjVqKg+eg=; AWSALBCORS=+WI8Wx2j/XyUk8QfsJHA/758M3p9zmV0AWjJwdqjZmVw5QkLNReMN6iLNHJJrnW/ZyxBtb3zjteTo53Msadc0Qz8Ud1AHjaw7sAchyOxCz7kyCU9r5pBeRT9SEMFuEFVXjQ9jXxjn40TTIy3lqTtPc10h0VI1TFTons/u6oNmcZ8x/m3EXWFYxUDvHvNTlIBw0Cpn/eimo58LIxlpH2I/RNo1RMvlOWzQh0BAtuFvrN8N7p+hPqXRORjVqKg+eg=",
+          "user-agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
         },
         referrerPolicy: "strict-origin-when-cross-origin",
         body: null,
@@ -37,7 +39,9 @@ async function handler() {
       },
     );
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const json = await response.json();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     console.log({ json });
 
     // const response = await fetch(env.DISCOVER_FEED_URL, {
